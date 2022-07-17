389895333d204dae88afcaa07945809cdb0217f2
NeerajSaini12560 <neerajsaini12560@gmail.com>
2022-07-17 17:59:54 +0530

    initial commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./../components/Header";
+import ShopSection from "./../components/homeComponents/ShopSection";
+import ContactInfo from "./../components/homeComponents/ContactInfo";
+import CalltoActionSection from "./../components/homeComponents/CalltoActionSection";
+
+
+const HomeScreen = () => {
+  window.scrollTo(0, 0);
+  return (
+    <div>
+      <Header />
+      <ShopSection />
+      <CalltoActionSection />
+      <ContactInfo />
+    </div>
+  );
+};
+
+export default HomeScreen;
